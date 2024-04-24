dc8bff5ee4c92fc83fddc09672058a00ae7414ea
forzys <zys@outlook.in>
2024-04-24 16:23:35 +0800

    change app icon and add package

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.jsx', '.ts', '.tsx', '.json'],
+        alias: {
+          '@': ['./src']
+        }
+      }
+    ],
+    ['react-native-worklets-core/plugin'],
+    'react-native-reanimated/plugin'
+  ], 
 };
